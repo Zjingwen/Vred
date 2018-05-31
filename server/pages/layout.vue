1c78175e7c03341433c9065c2676288f4b08732c
静文（jingwen） <jingwen@xiaoshijie.com>
2018-06-01 02:25:05 +0800

    release * add layout.page demo right-sider

--- server/pages/layout.vue
+++ server/pages/layout.vue
@@ -48,62 +48,65 @@
                 <textarea slot="markdown" v-html="leftSiferMd"></textarea>
                 <textarea slot="code" v-html="leftSiferHtml"></textarea>
             </t-exapmple>
-            <t-layout>
-                <t-sider>sider</t-sider>
-                <t-layout class='demo-content'>
-                    <t-header>header</t-header>    
-                    <t-content>
-                        <p v-for="i in 10" :key='i' v-text='i'></p>
-                    </t-content>
-                    <t-footer>footer</t-footer>
-                </t-layout>
-            </t-layout>
             <br>
             <h1>右sider</h1>
-            <t-header>header</t-header>
-            <t-layout class='demo-content'>
-                <t-content>
-                    <p v-for="i in 10" :key='i' v-text='i'></p>
-                </t-content>
-                <t-sider width="400">sider</t-sider>
-            </t-layout>
-            <t-footer>footer</t-footer>
+            <t-exapmple more>
+                <div slot="view">
+                    <t-header>header</t-header>
+                        <t-layout class='demo-content'>
+                            <t-content>
+                                <p v-for="i in 10" :key='i' v-text='i'></p>
+                            </t-content>
+                            <t-sider width="400">sider</t-sider>
+                        </t-layout>
+                    <t-footer>footer</t-footer>
+                </div>
+                <textarea slot="markdown" v-html="rightSiferMd"></textarea>
+                <textarea slot="code" v-html="rightSiferHtml"></textarea>
+            </t-exapmple>
+            
         </contents>
         <footers></footers>
     </div>
 </template>
 <style lang="css" scoped>
-    .v-header,.v-footer{
-        text-align: center;
-        height: 36px;
-        line-height: 36px;
-        background: #b3c0d1;
-        color: #FFF;
-        font-size: 12px;
-    }
-    .v-sider{
-        background: rgba(0,153,229,.7);
-    }
-    .v-content{
-        background: rgba(0, 117, 175, 0.9);
-    }
+.v-header,
+.v-footer {
+  text-align: center;
+  height: 36px;
+  line-height: 36px;
+  background: #b3c0d1;
+  color: #fff;
+  font-size: 12px;
+}
+.v-sider {
+  background: rgba(0, 153, 229, 0.7);
+}
+.v-content {
+  background: rgba(0, 117, 175, 0.9);
+}
 </style>
 <script>
-    import noSiferHtml from '../doc/layout/no-sider.html';
-    import noSiferMd from '../doc/layout/no-sider.md';
-    
-    import leftSiferHtml from '../doc/layout/left-sider.html';
-    import leftSiferMd from '../doc/layout/left-sider.md';
+import noSiferHtml from "../doc/layout/no-sider.html";
+import noSiferMd from "../doc/layout/no-sider.md";
 
-    export default{
-        data:function(){
-            return {
-                linkName: '布局',
-                noSiferHtml: noSiferHtml,
-                noSiferMd: noSiferMd,
-                leftSiferHtml: leftSiferHtml,
-                leftSiferMd: leftSiferMd
-            }
-        }
-    }
+import leftSiferHtml from "../doc/layout/left-sider.html";
+import leftSiferMd from "../doc/layout/left-sider.md";
+
+import rightSiferHtml from "../doc/layout/right-sider.html";
+import rightSiferMd from "../doc/layout/right-sider.md";
+
+export default {
+  data: function() {
+    return {
+      linkName: "布局",
+      noSiferHtml: noSiferHtml,
+      noSiferMd: noSiferMd,
+      leftSiferHtml: leftSiferHtml,
+      leftSiferMd: leftSiferMd,
+      rightSiferHtml: rightSiferHtml,
+      rightSiferMd: rightSiferMd
+    };
+  }
+};
 </script>
