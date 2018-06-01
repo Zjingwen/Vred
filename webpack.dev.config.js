ab2f31070a7d7e5a9be48e46164a79aeea4dce05
静文（jingwen） <jingwen@xiaoshijie.com>
2018-06-02 01:29:53 +0800

    release * change webpack config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,55 +1,57 @@
 let path = require('path');
 let webpack = require('webpack');
-const { VueLoaderPlugin } = require('vue-loader')
+const {VueLoaderPlugin} = require('vue-loader');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        lanlanUI: path.resolve(__dirname, 'components/component.js'),
-        index: path.resolve(__dirname,'server/index.js')
-    },
-    output: {
-        path: path.resolve(__dirname, 'server/lib'),
-        filename: '[name].js'
-    },
-    devtool: 'inline-source-map',
-    devServer:{
-        contentBase: path.join(__dirname,'server'),
-        compress: true,
-        port: 9000
-    },
-    module: {
-        rules: [
-            {test: /\.css$/,use:[
-                "vue-style-loader",
-                "style-loader",
-                "css-loader"
-            ]},
-            {test: /\.html$/, loader: 'html-loader'},
-            {test: /\.vue$/, loader: 'vue-loader'},
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.md$/,
-                exclude: /node_modules/,
-                use:[{
-                        loader: "html-loader"
-                    },{
-                        loader: "markdown-loader",
-                    }]
-            },
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.css']
-    },
-    plugins: [
-        new webpack.HashedModuleIdsPlugin(),
-        new VueLoaderPlugin()
-    ]
-}
+  mode: 'development',
+  entry: {
+    lanlanUI: path.resolve(__dirname, 'components/component.js'),
+    index: path.resolve(__dirname,'server/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, 'server/lib'),
+    filename: '[name].js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'server'),
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {test: /\.html$/, loader: 'html-loader'},
+      {test: /\.vue$/, loader: 'vue-loader'},
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.md$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'html-loader',
+        }, {
+          loader: 'markdown-loader',
+        }],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.css'],
+  },
+  plugins: [
+    new webpack.HashedModuleIdsPlugin(),
+    new VueLoaderPlugin(),
+  ],
+};
--- webpack.pro.config.js
+++ webpack.pro.config.js
@@ -1,39 +1,40 @@
 let path = require('path');
 let webpack = require('webpack');
-const { VueLoaderPlugin } = require('vue-loader')
+const {VueLoaderPlugin} = require('vue-loader');
 
 module.exports = {
-    mode: 'production',
-    entry: {
-        lanlanUI: path.resolve(__dirname, 'components/component.js'),
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
-    },
-    module: {
-        rules: [
-            {test: /\.css$/,use:[
-                "vue-style-loader",
-                "style-loader",
-                "css-loader"
-            ]},
-            {test: /\.html$/, loader: 'html-loader'},
-            {test: /\.vue$/, loader: 'vue-loader'},
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.css']
-    },
-    plugins: [
-        new webpack.HashedModuleIdsPlugin(),
-        new VueLoaderPlugin()
-    ]
-}
+  mode: 'production',
+  entry: {
+    lanlanUI: path.resolve(__dirname, 'components/component.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'style-loader',
+          'css-loader',
+        ]},
+      {test: /\.html$/, loader: 'html-loader'},
+      {test: /\.vue$/, loader: 'vue-loader'},
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.css'],
+  },
+  plugins: [
+    new webpack.HashedModuleIdsPlugin(),
+    new VueLoaderPlugin(),
+  ],
+};
