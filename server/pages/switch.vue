d369d22175df13313edf039082580ba81e679461
静文（jingwen） <jingwen@xiaoshijie.com>
2018-06-28 01:09:42 +0800

    release * change switch

--- server/pages/switch.vue
+++ server/pages/switch.vue
@@ -3,7 +3,12 @@
         <headers></headers>
         <contents :linkName='linkName'>
           <h1>开关</h1>
-          <p>常用模式</p>
+          <h2>常用模式</h2>
+          <t-Switch v-model="switchBase" @on-change='change'></t-Switch>
+          <p>switch：{{switchBase}}</p>
+          <h2>disabled</h2>
+          <t-Switch v-model="switchDisabled" disabled></t-Switch>
+          <p>switch：{{switchDisabled}}</p>
         </contents>
         <footers></footers>
     </div>
@@ -14,9 +19,14 @@
         data:function(){
             return {
                 linkName:'开关',
+                switchBase: true,
+                switchDisabled: true,
             }
         },
         methods:{
+            change:function(val){
+                this.$message(`switch emit : ${val}`)
+            }
         }
     }
 </script>
