dedf9648052e8d3e9fe4021650095cea6419887d
静文（jingwen） <jingwen@xiaoshijie.com>
2018-07-10 10:42:43 +0800

    release * change date-picker

---
+++ components/datetime/date-picker.vue
@@ -0,0 +1,137 @@
+<template>
+  <t-input
+    ref="reference"
+    :disabled="disabled"
+    :readonly="readonly"
+    :size="size"
+    :placeholder="placeholder"
+    :value="displayValue"
+    v-clickoutside="handleClose"
+    @keydown.native="handleKeydown"
+    @on-focus="handleFocus"
+    @blur="handleBlur"
+    @change.native="displayValue = $event.target.value"
+  />
+</template>
+<script>
+import vuePopper from '@directives/vue-popper.js';
+import Clickoutside from '@directives/clickoutside.js';
+let datePicker = require('./date.vue');
+
+export default {
+  mixins: [vuePopper],
+  directives: {
+    clickoutside: Clickoutside,
+  },
+  props: {
+    size: String,
+    format: String,
+    readonly: Boolean,
+    placeholder: String,
+    disabled: Boolean,
+    value: {},
+    autoClose: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  data: function() {
+    return {
+      pickerVisible: false,
+      currentValue: '',
+    };
+  },
+  created: function() {
+    this.panel = datePicker.default;
+  },
+  watch: {
+    pickerVisible: function(val) {
+      // if (this.readonly || this.disabled) return;
+      if (this.disabled) return;
+      val ? this.showPicker() : this.hidePicker();
+    },
+    currentValue: function(val) {
+      // if (val) return;
+      this.$emit('input', val);
+    },
+    value: {
+      immediate: true,
+      handler: function(val) {
+        // this.currentValue = util.isDate(val) ? new Date(val) : val;
+        this.currentValue = val;
+      },
+    },
+    displayValue: function(val) {
+      this.$emit('change', val);
+    },
+  },
+  computed: {
+    displayValue: {
+      get: function() {
+        return this.currentValue;
+      },
+      set: function(value) {
+        if (value.indexOf(':') > 0) {
+          this.picker.value = value.split(':');
+        } else {
+          this.picker.value = value;
+        }
+      },
+    },
+    refInput: function() {
+      if (this.$el) return this.$el.querySelector('input');
+      return {};
+    },
+  },
+  methods: {
+    hidePicker: function() {
+      if (this.picker) {
+        this.picker.visible = false;
+        this.destroyPopper();
+      }
+    },
+    showPicker: function() {
+      if (!this.picker) {
+        // this.panel.defaultValue = this.currentValue;
+        this.panel.autoClose = this.autoClose;
+        this.picker = new Vue(this.panel).$mount(document.createElement('div'));
+
+        this.popperEl = this.picker.$el;
+        this.referenceEl = this.$refs.reference.$el;
+
+        this.$el.appendChild(this.picker.$el);
+        this.pickerVisible = this.picker.visible = true;
+
+        this.picker.$on('dodestroy', this.handleClose);
+        this.picker.$on('pick', function(date) {
+          this.$emit('input', date);
+        }.bind(this));
+      } else {
+        this.pickerVisible = this.picker.visible = true;
+      }
+
+      this.updatePopper();
+
+      this.picker.value = this.currentValue;
+    },
+    handleKeydown: function(event) {
+      let keyCode = event.keyCode;
+      // tab
+      if (keyCode === 9) {
+        this.pickerVisible = false;
+      }
+    },
+    handleFocus: function() {
+      this.pickerVisible = true;
+      this.$emit('focus', this);
+    },
+    handleBlur: function() {
+      this.$emit('blur', this);
+    },
+    handleClose: function() {
+      this.pickerVisible = false;
+      this.refInput.blur();
+    },
+  },
+};
+</script>
---
+++ server/pages/datePicker.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <headers></headers>
+    <contents :linkName='linkName'>
+      <div style="width:244px;">
+        <t-datePicker v-model='dateValue' />
+      </div>
+    </contents>
+    <footers></footers>
+  </div>
+</template>
+<style lang="css"></style>
+<script>
+  export default{
+    data:function(){
+      return {
+        linkName:'日期选择',
+        dateValue: ''
+      }
+    },
+  }
+</script>
