48156de9fc3e56a012f821f85ee9fdbe3b856521
静文（jingwen） <jingwen@xiaoshijie.com>
2018-07-12 00:04:04 +0800

    feature * add row props test

--- tests/row.js
+++ tests/row.js
@@ -6,4 +6,46 @@ describe('row', () => {
     const vm = mount(row);
     expect(vm.name()).toBe('v-row');
   });
+
+  it('props-gutter', ()=>{
+    const vm = mount(row, {
+      propsData: {
+        gutter: 1,
+      },
+    });
+    expect(vm.props().gutter).toBe(1);
+  });
+
+  it('props-type', ()=>{
+    const vm = mount(row, {
+      propsData: {
+        type: 'flex',
+      },
+    });
+    expect(vm.props().type).toBe('flex');
+  });
+
+  it('props-justify', ()=>{
+    let list = ['', 'start', 'end', 'center', 'space-around', 'space-between'];
+    list.forEach((element)=>{
+      const vm = mount(row, {
+        propsData: {
+          justify: element,
+        },
+      });
+      expect(vm.props().justify).toBe(element);
+    });
+  });
+
+  it('props-align', ()=>{
+    let list = ['', 'top', 'middle', 'bottom'];
+    list.forEach((element)=>{
+      const vm = mount(row, {
+        propsData: {
+          align: element,
+        },
+      });
+      expect(vm.props().align).toBe(element);
+    });
+  });
 });
